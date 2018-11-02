8ac8101d2f31173127189073f9638744c1c357e2
Caio Augusto Silva <caiosilva50@gmail.com>
2018-11-02 02:51:48 -0300

    Adição do calculo V2

--- api-server.py
+++ api-server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_script import Server, Manager
-from Calculo_v1 import Calculo
+import Calculo_v1
+import Calculo_v2
 from flaskext.mysql import MySQL
 import simplejson
 
@@ -17,8 +18,8 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-@app.route('/calcula', methods=['POST'])
-def calcula():
+@app.route('/v1', methods=['POST'])
+def calcula_v1():
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
@@ -26,11 +27,29 @@ def calcula():
         entrada = [request.form.get('v1'), request.form.get('v2'), request.form.get('v3'), request.form.get('v4'),
                    request.form.get('v5'), request.form.get('v6'), request.form.get('v7'), request.form.get('v8'),
                    request.form.get('v9'), request.form.get('v10')]
-        results = Calculo(cursor, precoMax, entrada).resultado()
+        results = Calculo_v1.Calculo(cursor, precoMax, entrada).resultado()
         return simplejson.dumps({'resultado': results})
 
     except Exception as e:
         return simplejson.dumps({'erro': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/v2', methods=['POST'])
+def calcula_v2():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        precoMax = request.form.get('precoMax')
+        entrada = [int(request.form.get('camera')), int(request.form.get('desempenho')), int(request.form.get('tela')),
+                   int(request.form.get('bateria')), int(request.form.get('armazenamento'))]
+        results = Calculo_v2.Calculo(cursor, precoMax, entrada).resultado()
+        return simplejson.dumps({'resultado': results})
+
+    except Exception as e:
+        return simplejson.dumps({'erro': str(e)}), 403
     finally:
         cursor.close()
         conn.close()
