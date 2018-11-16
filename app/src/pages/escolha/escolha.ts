2c9d80a62a3679dbd5979503e50f3d6781d61447
RafaelJM <rfel.jm@gmail.com>
2018-11-16 16:27:17 -0200

    escolha v2, comecando a parte de comunicacao com a api

--- app/src/pages/escolha/escolha.ts
+++ app/src/pages/escolha/escolha.ts
@@ -2,18 +2,38 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { MelhorCelularPage } from './../melhorcelular/melhorcelular';
 import { SobrePistacheiPage } from './../sobrepistachei/sobrepistachei';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'page-escolha',
   templateUrl: 'escolha.html'
 })
 export class EscolhaPage {
-  constructor(public navCtrl: NavController) {
+
+  dados = this.formBuilder.group({
+    precoMax: [800],
+    camera: [],
+    desempenho: [],
+    tela: [],
+    bateria: [],
+    armazenamento: []
+  });
+
+  constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {
 
   }
 
   public goMelhorCelularPage(){
-    this.navCtrl.push(MelhorCelularPage, {}, {animate: true, animation:'pingu', direction: 'forward'});
+    let data = {
+      "precoMax": this.dados.controls.precoMax,
+      "camera": this.dados.controls.camera,
+      "desempenho": this.dados.controls.desempenho,
+      "tela": this.dados.controls.tela,
+      "bateria": this.dados.controls.bateria,
+      "armazenamento": this.dados.controls.armazenamento,
+    }
+
+    this.navCtrl.push(MelhorCelularPage, {dados: this.dados}, {animate: true, animation:'pingu', direction: 'forward'});
   };
   public goSobrePistacheiPage(){
     this.navCtrl.push(SobrePistacheiPage, {}, {animate: true, animation:'pingu', direction: 'forward'});
