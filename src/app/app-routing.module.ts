1df239e102f229358a8ee092d022c72d10f6afb6
Mateus Barros <mateush81@gmail.com>
2019-03-05 21:24:22 +0000

    Tela para todos os celulares

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
   {
     path: 'chat',
     loadChildren: './chat/chat.module#ChatPageModule'
-  },  { path: 'celulares', loadChildren: './celulares/celulares.module#CelularesPageModule' }
+  },  { path: 'celulares', loadChildren: './celulares/celulares.module#CelularesPageModule' },
+  { path: 'lista-celulares', loadChildren: './lista-celulares/lista-celulares.module#ListaCelularesPageModule' }
 
   // ,
   // {
---
+++ src/app/lista-celulares/lista-celulares.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ListaCelularesPage } from './lista-celulares.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaCelularesPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ListaCelularesPage]
+})
+export class ListaCelularesPageModule {}
